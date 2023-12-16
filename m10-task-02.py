63258f7931fbf00f09fc8f74396c22020ef6f6e3
ingyaroslav <ingyaroslav@gmail.com>
2023-12-16 19:23:51 +0200

    ..

--- m10-task-02.py
+++ m10-task-02.py
@@ -12,12 +12,9 @@ class Name(Field):
 
 class Phone(Field):
     def __init__(self, value):
+        if not (value.isdigit() and len(value) == 10):
+            raise ValueError("Invalid phone number format.")
         super().__init__(value)
-        self.validate_phone()
-
-    def validate_phone(self):
-        if len(self.value) != 10 or not self.value.isdigit():
-            raise ValueError("Invalid phone number. Phone number must be a 10-digit number.")
 
 
 class Record:
@@ -29,22 +26,21 @@ class Record:
         self.phones.append(Phone(phone))
 
     def remove_phone(self, phone):
-        self.phones = [p for p in self.phones if str(p) != phone]
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+                return
 
     def edit_phone(self, old_phone, new_phone):
-        for i, phone in enumerate(self.phones):
-            if str(phone) == old_phone:
-                self.phones[i] = Phone(new_phone)
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+                return
 
     def find_phone(self, phone):
         for p in self.phones:
-            if str(p) == phone:
-                return p
-        return None
-
-    def __str__(self):
-        phone_numbers = "; ".join(str(p) for p in self.phones)
-        return f"Contact name: {self.name}, phones: {phone_numbers}"
+            if p.value == phone:
+                return p.value
 
 
 class AddressBook:
@@ -52,11 +48,65 @@ class AddressBook:
         self.data = {}
 
     def add_record(self, record):
-        name = str(record.name)
-        self.data[name] = record
+        self.data[record.name.value] = record
 
     def find(self, name):
         return self.data.get(name)
 
     def delete(self, name):
-        del self.data[name]
+        if name in self.data:
+            del self.data[name]
+
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, args
+
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, args = parse_input(user_input)
+
+        try:
+            if command in ["close", "exit"]:
+                print("Good bye!")
+                break
+            elif command == "hello":
+                print("How can I help you?")
+            elif command == "add":
+                name, phone = args
+                record = Record(name)
+                record.add_phone(phone)
+                contacts[name] = record
+                print("Contact added.")
+            elif command == "change":
+                name, phone = args
+                if name in contacts:
+                    contacts[name].edit_phone(contacts[name].phones[0].value, phone)
+                    print("Contact updated.")
+                else:
+                    print(f"Contact with name '{name}' not found.")
+            elif command == "phone":
+                name = args[0]
+                if name in contacts:
+                    print(contacts[name].phones[0].value)
+                else:
+                    print(f"Contact with name '{name}' not found.")
+            elif command == "all":
+                if contacts:
+                    for name, record in contacts.items():
+                        print(f"{name}: {record.phones[0].value}")
+                else:
+                    print("No contacts available.")
+            else:
+                print("Invalid command.")
+        except ValueError as e:
+            print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
